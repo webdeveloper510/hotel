2084f29fe4f2b8a6654f50be4d36b3ecac3935b7
Gurpreet Kaur <gurpreetkaur@codenomad.net>
2023-06-14 01:19:42 +0530

    changes

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -266,7 +266,9 @@ class UserController extends Controller
             $stripe = new \Stripe\StripeClient([
                 'api_key' => env('STRIPE_SECRET'),
             ]);
-
+            echo "<pre>";
+            print_r($stripe);
+            die;
             $token = $stripe->tokens->create([
                 'card' => [
                     'number' => $request->number,
