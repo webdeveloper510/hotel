7f54f46d4869035525c784e11d82b7bf27ac80bb
ankushsaini@codenomad.net <ankushsaini@codenomad.net>
2023-06-14 00:04:47 +0530

    changes

--- routes/api.php
+++ routes/api.php
@@ -24,14 +24,18 @@ Route::post('hotel-location', [UserController::class, 'hotel_location']);
 Route::post('add-rooms', [UserController::class, 'add_room']);
 Route::get('get-hotel-location', [UserController::class, 'get_hotel_location']);
 
-Route::post('get-rooms', [UserController::class, 'get_rooms']);
+Route::get('get-rooms', [UserController::class, 'get_rooms']);
 Route::post('update-rooms/{id}', [UserController::class, 'update_rooms']);
 Route::post('delete-rooms/{id}', [UserController::class, 'delete_rooms']);
 
+
 Route::post('stripe', [UserController::class, 'stripePost']);
+
+Route::get('search', [UserController::class, 'search_hotels']);
 
 Route::group(['middleware' => 'auth:api'], function () {
   Route::post('user-details', [UserController::class, 'userDetails']);
   Route::get('get-users', [UserController::class, 'getUsers']);
   Route::post('user-logout', [UserController::class, 'logout']);
+  Route::post('book-hotel', [UserController::class, 'book_hotel']);
 });
